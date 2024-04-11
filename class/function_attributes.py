ec6c86b38f63f5197c02d4ecc293320a82e2e0a1
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-04-11 21:41:58 +0300

    Add a solution

---
+++ class/function_attributes.py
@@ -0,0 +1,7 @@
+# Define a Python function student().
+# Using function attributes display the names of all arguments.
+def student(name, grades):
+    return f"Student name: {name}. Grades: {grades}"
+
+
+print(student("Maria", "6, 6, 5"))
