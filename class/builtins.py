25fe424597bd4e2d17a7f5514f954fa9f6a33824
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-04-11 18:34:11 +0300

    Add a solution

---
+++ class/builtins.py
@@ -0,0 +1,8 @@
+# Write a Python program that imports the abs() function using the builtins module,
+# displays the documentation of the abs() function and
+# finds the absolute value of -155.
+
+import builtins
+
+help(builtins.abs)
+print(builtins.abs(-155))
