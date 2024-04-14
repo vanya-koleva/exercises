1c4610a275043f686a6de523608b73376f81335e
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-04-14 22:26:40 +0300

    Add a solution

---
+++ dictionaries/print_a_dict.py
@@ -0,0 +1,5 @@
+# Write a Python program to print a dictionary line by line.
+my_dict = {1: "Naruto", 2: "Bleach", 3: "Death Note"}
+for key, value in my_dict.items():
+    print(f"{key} : {value}")
+    
