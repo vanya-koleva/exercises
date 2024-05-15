035c2d400e7f71274385fd4eb6bee163cffc52b6
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-15 23:20:56 +0300

    Add a solution

---
+++ datetime_exercises/02_leap_year.py
@@ -0,0 +1,12 @@
+def check_leap_year(y):
+    if y % 400 == 0:
+        return True
+    if y % 100 == 0:
+        return False
+    if y % 4 == 0:
+        return True
+    else:
+        return False
+
+
+print(check_leap_year(2024))
