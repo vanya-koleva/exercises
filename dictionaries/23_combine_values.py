5c7aeb3c90abc075dea012ce1e5d17a013c000a7
vanya-koleva <148770184+vanya-koleva@users.noreply.github.com>
2024-05-30 16:21:58 +0300

    Add a solution

---
+++ dictionaries/23_combine_values.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+sample_data = [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 300}, {'item': 'item1', 'amount': 750}]
+result = Counter()
+
+for d in sample_data:
+    result[d["item"]] += d["amount"]
+
+print(result)
